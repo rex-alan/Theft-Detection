17c43cc21bd9c20e2e6efc5e5bca6a305b52667e
rex-alan <rexalan1174@gmail.com>
2022-08-04 06:49:57 +0530

    Capturing criminal face

---
+++ capture.py
@@ -0,0 +1,27 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("test")
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        print('Image' + str(img_counter) + 'saved')
+        file = 'X:/mano/theft detection/Training_images/Edwin.jpg'
+        cv2.imwrite(file, frame)
+        img_counter += 1
+
+cam.release()
+cv2.destroyAllWindows()
